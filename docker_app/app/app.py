e8f1fbb6655b97a0d7c97e6608391c2a4fe74941
ayushpatel-srijan <108351365+ayushpatel-srijan@users.noreply.github.com>
2022-11-02 18:48:13 +0530

    Update app.py

--- docker_app/app/app.py
+++ docker_app/app/app.py
@@ -1,12 +1,13 @@
 from unicodedata import name
 import streamlit as st
 import os
-from fun import get_chapters, group,pretty , convert ,split_video,abstractive_model_output,extract_chapters,add_phrases,get_phrases,extract_with_yake
+from fun import *
 import pickle
 import shutil
-
 from nltk.tokenize import word_tokenize, sent_tokenize
-import nltk   
+import nltk
+nltk.download('punkt')
+from nltk import sent_tokenize
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'><u>Video Segmentation</u></h1>", unsafe_allow_html=True)
 
@@ -14,7 +15,7 @@ st.markdown("<h1 style='text-align: center;'><u>Video Segmentation</u></h1>", un
 def show_vid(i):
 
     if len(vid_list) !=0:
-        vid_path=vid_list[i][1]
+        vid_path = vid_list[i][1]
     j=i
 
 vf=""
@@ -32,12 +33,10 @@ with col1:
     video_file = st.file_uploader('video', type = ['mp4'])
     if video_file:
       
-        st.success('Done!')
         if not os.path.exists("videos"):
             os.mkdir("videos")
         with open(os.path.join("videos",f"{video_file.name}"),"wb") as f:
             f.write(video_file.getbuffer())         
-            st.success("Saved File")
 
         if not os.path.exists(os.path.join("chapters",f'{video_file.name[:-4]}_chapters.pkl')):
             extract_chapters(video_file.name)
@@ -50,7 +49,10 @@ with col1:
         vid_list =split_video(video_file.name,chaps)
         st.video(video_file)
         vf=video_file.name[:-4]
-        #show = st.button("Show Chapters")
+        st.header("Summary")
+        if os.path.exists(os.path.join("Video summary",f'{video_file.name[:-4]}_video_summary.pkl')):
+            video_summary = get_video_summary(video_file.name)
+            st.markdown(video_summary[0]['summary'])
 
 
 with col2:
@@ -58,7 +60,7 @@ with col2:
     for i in range(len(chaps)):
         v = st.button(on_click = show_vid(i),
                 key=i,
-                label=f"{pretty(chaps[i][2].split('>')[-1])} ➤")
+                label=f"{pretty(chaps[i][2].split('>')[1])} ➤")
         if v:
             j=i
             topic_full=chaps[i][2].split('>')
@@ -69,17 +71,17 @@ with col2:
 
 with col3:
     if vid_path!="":
-        st.header(label)
+        st.header("-".join(label.split("-")[:2])) 
         print("-->",vid_path)
         st.video(vid_path)
-        st.header(str("Summary ("+convert(chaps[j][0]/1000)+" - "+convert(chaps[j][1]/1000)+")"))
+        st.header(str("Clip Summary ("+convert(chaps[j][0]/1000)+" - "+convert(chaps[j][1]/1000)+")"))
         if len(chaps[j][4])!=0 :
             phrase = " , ".join([i for i in sorted(chaps[j][4], key=len)[-2:]])
         else:
             phrase=extract_with_yake(chaps[j][3])
-        
+        print("tokeized")
         st.subheader(f"Related to : {phrase.title()}")
-        sentences = nltk.sent_tokenize(chaps[j][3])
+        sentences = sent_tokenize(chaps[j][3])
         #for ind ,points in enumerate(chaps[j][3].split(".")[:-1]):
         for ind ,points in enumerate(sentences):
             st.markdown(str(str(ind+1) +". "+points))
